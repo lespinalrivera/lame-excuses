110d3d86ee8f1352838ee6a758e458c473a178cb
Luis Espinal <lespinalrivera@gmail.com>
2017-06-12 23:07:11 -0700

    Adding test environment with mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var lameExcuses = require('./index');
+
+describe('lame-excuses', function() {
+  it('should work!', function() {
+    expect(true).to.be.true;
+  });
+});
