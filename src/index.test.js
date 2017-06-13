6ec86badf18b30e11097d0394579bfd7459bfb67
Luis Espinal <lespinalrivera@gmail.com>
2017-06-13 13:09:38 -0700

    Adding testing environment for random

--- src/index.test.js
+++ src/index.test.js
@@ -5,4 +5,10 @@ describe('lame-excuses', function() {
   it('should work!', function() {
     expect(true).to.be.true;
   });
+  describe('random', function() {
+    it('should return a random item from lameExcuses.all', function() {
+      var randomItem = lameExcuses.random();
+      expect(lameExcuses.all).to.include(randomItem);
+    });
+  });
 });
