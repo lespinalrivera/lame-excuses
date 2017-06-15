6dcd75b10b7f18e9b7db2c8a95cf89894d5ac174
Luis Espinal <lespinalrivera@gmail.com>
2017-06-14 22:02:51 -0700

    feat(random): adding a random function to get more than one lame excuse
    Function to get more than one lame excuse, as many as you need to excuse yourself :star:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqueRandomArray = require('unique-random-array');
 var lameExcuses = require('./lame-excuses.json');
+var getRandomItem = uniqueRandomArray(lameExcuses);
 
 module.exports = {
   all: lameExcuses,
-  random: uniqueRandomArray(lameExcuses)
+  random: random
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i =0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -10,5 +10,14 @@ describe('lame-excuses', function() {
       var randomItem = lameExcuses.random();
       expect(lameExcuses.all).to.include(randomItem);
     });
+
+    it('should return an array of items if passed a number', function(){
+      var randomItems = lameExcuses.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(lameExcuses.all).to.include(item);
+      });
+    });
+
   });
 });
